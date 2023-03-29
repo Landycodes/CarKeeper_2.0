d05e33e1b00b725f59bfd86799635af407a09cc3
landycodes <landryandrewsk8@gmail.com>
2023-03-29 02:39:33 -0700

    signup creates user for database

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,13 +8,32 @@ export default function Login() {
   const [form, setForm] = useState(true);
   const router = useRouter();
 
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   const handleLogin = (event) => {
     event.preventDefault();
     router.push("/home");
   };
 
-  const handleSignUp = (event) => {
+  const handleSignUp = async (event) => {
     event.preventDefault();
+    console.log(formData);
+    await fetch("/api/newuser", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
   };
 
   return (
@@ -36,14 +55,37 @@ export default function Login() {
           </button>
         </form>
       ) : (
-        <form className="bg-dark rounded p-3 w-25 d-flex flex-column">
+        <form
+          className="bg-dark rounded p-3 w-25 d-flex flex-column"
+          onSubmit={handleSignUp}
+        >
           <h2 className="p-1">Sign Up</h2>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" />
+          <input
+            id="username"
+            name="username"
+            type="text"
+            onChange={handleInputChange}
+            defaultValue={formData.username}
+            required
+          />
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" />
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={handleInputChange}
+            defaultValue={formData.email}
+            required
+          />
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={handleInputChange}
+            defaultValue={formData.password}
+          />
           <button className="btn btn-success mt-2 mb-2">Create Account</button>
           <button className="btn btn-primary" onClick={() => setForm(!form)}>
             have an account? Login
