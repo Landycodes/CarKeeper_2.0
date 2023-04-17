a67d0abba57d6cbd4a14a80550d25d059756a1b7
landycodes <landryandrewsk8@gmail.com>
2023-04-16 17:33:34 -0700

    fixing textwrap on logout button

--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -22,7 +22,7 @@ export default function Nav({ title }) {
     return (
       <div className="w-100 text-light mb-3 text-center">
         <nav className="d-flex justify-content-around align-items-center">
-          <h5 className="logout" onClick={() => Auth.logout()}>
+          <h5 className="logout text-nowrap" onClick={() => Auth.logout()}>
             Log Out
           </h5>
           <h1>{title}</h1>
