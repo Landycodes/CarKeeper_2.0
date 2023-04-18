b321e52b4a9d15b97635ea76d67984dea8b7d270
landycodes <landryandrewsk8@gmail.com>
2023-04-18 15:19:56 -0700

    part status layout and functionality

--- src/pages/Status/index.js
+++ src/pages/Status/index.js
@@ -6,6 +6,24 @@ export default function Status() {
   //true = brake false = tread
   const [toggle, setToggle] = useState(true);
 
+  const [saveEdit, setButton] = useState(false);
+
+  const values = () => {
+    return toggle ? (
+      <div id="FL-brake" className={`d-flex w-100 ${saveEdit ? "" : "d-none"}`}>
+        <input className="mb-1 w-50 rounded brake-val" />
+        <input className="mb-1 w-50 rounded brake-val" />
+      </div>
+    ) : (
+      <div id="FL-tread" className={`d-flex ${saveEdit ? "" : "d-none"}`}>
+        <input className="p-1 mb-1 w-25 rounded tread-val" />
+        <input className="p-1 mb-1 w-25 rounded tread-val" />
+        <input className="p-1 mb-1 w-25 rounded tread-val" />
+        <input className="p-1 mb-1 w-25 rounded tread-val" />
+      </div>
+    );
+  };
+
   return (
     <Layout>
       <Nav title="Part Status" />
@@ -32,6 +50,7 @@ export default function Status() {
           className="d-none"
           onClick={() => {
             setToggle(!toggle);
+            setButton(false);
           }}
         />
         <div
@@ -43,85 +62,69 @@ export default function Status() {
               <label>
                 <h1 className="p-2">FL</h1>
               </label>
-              <h5 className="m-0 mt-2">
-                <span id="FL"></span>
-                <br></br>
-                <small className="measurement"></small>
-              </h5>
-              <div id="FL-tread" className="d-flex">
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-              </div>
-              <div id="FL-brake" className="d-flex w-100">
-                <input className="mb-1 w-50 rounded brake-val" />
-                <input className="mb-1 w-50 rounded brake-val" />
+              <div className="d-flex flex-column align-items-center">
+                <h5 className="m-0 mt-2">
+                  <span id="FL"></span>
+                  <br></br>
+                  <small className="measurement">
+                    {toggle ? "MM" : "/32nds"}
+                  </small>
+                </h5>
+                {values()}
               </div>
             </div>
             <div className="w-25">
               <label>
                 <h1 className="p-2">FR</h1>
               </label>
-              <h5 className="m-0 mt-2">
-                <span id="FR"></span>
-                <br />
-                <small className="measurement"></small>
-              </h5>
-              <div id="FR-tread" className="d-flex">
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-              </div>
-              <div id="FR-brake" className="d-flex w-100">
-                <input className="mb-1 w-50 rounded brake-val" />
-                <input className="mb-1 w-50 rounded brake-val" />
+              <div className="d-flex flex-column align-items-center">
+                <h5 className="m-0 mt-2">
+                  <span id="FR"></span>
+                  <br />
+                  <small className="measurement">
+                    {toggle ? "MM" : "/32nds"}
+                  </small>
+                </h5>
+                {values()}
               </div>
             </div>
           </div>
           <div className="m-0 w-100 h-25 d-flex align-items-center justify-content-center">
-            <button id="edit-stat" className="enter text-white">
-              Edit
+            <button
+              id="edit-stat"
+              className="enter text-white"
+              onClick={() => setButton(!saveEdit)}
+            >
+              {saveEdit ? "Save" : "Edit"}
             </button>
           </div>
           <div className="w-100 h-25 mb-2 d-flex justify-content-between align-items-end">
             <div className="w-25">
-              <div id="BL-brake" className="d-flex w-100">
-                <input className="mb-1 w-50 rounded brake-val" />
-                <input className="mb-1 w-50 rounded brake-val" />
+              <div className="d-flex flex-column align-items-center">
+                {values()}
+                <h5>
+                  <span id="BL"></span>
+                  <br />
+                  <small className="measurement">
+                    {toggle ? "MM" : "/32nds"}
+                  </small>
+                </h5>
               </div>
-              <div id="BL-tread" className="d-flex">
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-              </div>
-              <h5>
-                <span id="BL"></span>
-                <br />
-                <small className="measurement"></small>
-              </h5>
               <label>
                 <h1 className="p-2">BL</h1>
               </label>
             </div>
             <div className="w-25">
-              <div id="BR-brake" className="d-flex w-100">
-                <input className="mb-1 w-50 rounded brake-val" />
-                <input className="mb-1 w-50 rounded brake-val" />
+              <div className="d-flex flex-column align-items-center">
+                {values()}
+                <h5>
+                  <span id="BR"></span>
+                  <br />
+                  <small className="measurement">
+                    {toggle ? "MM" : "/32nds"}
+                  </small>
+                </h5>
               </div>
-              <div id="BR-tread" className="d-flex">
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-                <input className="p-1 mb-1 w-25 rounded tread-val" />
-              </div>
-              <h5>
-                <span id="BR"></span>
-                <br />
-                <small className="measurement"></small>
-              </h5>
               <label>
                 <h1 className="p-2">BR</h1>
               </label>
